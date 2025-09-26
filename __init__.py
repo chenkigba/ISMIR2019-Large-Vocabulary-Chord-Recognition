9e617478e098ad63697770bd7e9a5fc32e160424
Wuji Chen <chenwuji2000@gmail.com>
2025-09-26 20:31:04 +0800

    retry

--- __init__.py
+++ __init__.py
@@ -20,6 +20,15 @@ except ImportError:
     # Handle case where dependencies might not be available
     chord_recognition_main = None
 
+# Also make it available at module level for direct import
+try:
+    from chord_recognition import chord_recognition_main as _chord_recognition_main
+
+    if chord_recognition_main is None:
+        chord_recognition_main = _chord_recognition_main
+except ImportError:
+    pass
+
 __all__ = [
     "mir",
     "extractors",
